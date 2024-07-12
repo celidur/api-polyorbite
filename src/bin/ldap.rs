2ee45b6e64d195f0a269aa17fd789cb481507a46
celidur <66182659+celidur@users.noreply.github.com>
2024-07-12 17:16:33 -0400

    add start api

---
+++ src/bin/ldap.rs
@@ -0,0 +1,71 @@
+use std::vec;
+
+use dotenv::dotenv;
+
+use api_polyorbite::common::{Ldap, user::ModifyUser, user::UserBuilder};
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok(); // Load the .env file
+
+
+    let ldap = Ldap::new().await;
+    if ldap.is_err() {
+        panic!("{:?}", ldap.err().unwrap());
+    }
+    let mut ldap = ldap.unwrap();
+
+    // test_user(&mut ldap).await;    
+
+    // let groups = ldap.groups.to_vec().await;
+    // for group in groups {
+    //     if group.cn == "steporbite" {
+    //         println!("{:?}", group);
+    //     }
+    // }
+
+
+    let res = ldap.groups.add_group_owner("polyorbite", vec!["uid=gaetan.florio,ou=people,dc=polyorbite,dc=com"]).await;
+
+    let group = ldap.groups.group("polyorbite").await.unwrap();
+    println!("{:?}", group);
+}
+
+async fn test_user(ldap: &mut Ldap) {
+    let value = ldap.users.user("user_test").await.is_some();
+    let res = ldap.users.delete_user("user_test").await;
+    if res.is_err() {
+        panic!("{:?}", res.err().unwrap());
+    }
+    let res = res.unwrap();
+    println!("Delete user result: {} expected: {}", res, value);
+    
+    let new_user = UserBuilder::new().uid("user_test".to_string()).password("test".to_string()).mail("test".to_string()).last_name("test".to_string()).first_name("test".to_string()).name("test".to_string()).picture(vec![137, 80]).build();
+    if new_user.is_err() {
+        panic!("{:?}", new_user.err().unwrap());
+    }
+    let new_user = new_user.unwrap();
+
+    println!("new_user: {:?}", new_user);
+
+    let res = ldap.users.new_user(new_user).await;
+    if res.is_err() {
+        panic!("{:?}", res.err().unwrap());
+    }
+    let res = res.unwrap();
+
+    if res {
+        let user = ldap.users.user("user_test").await.unwrap();
+        println!("user: {:?}", user);
+    } else {
+        panic!("Failed to create user");
+    }
+
+    let modify = ModifyUser::new().password("password".to_string());
+    let res = ldap.users.modify_user("user_test", modify).await.unwrap();
+    println!("Modify user result: {}", res);
+
+    let user = ldap.users.user("user_test").await.unwrap();
+    println!("user: {:?}", user);
+    println!("verif : {}", user.verify_password("password"));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,18 @@
-use std::vec;
-
-use dotenv::dotenv;
-
-use crate::common::{Ldap, user::ModifyUser, user::UserBuilder};
-
-mod common;
+use axum::{response::Html, routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok(); // Load the .env file
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
 
-
-    let ldap = Ldap::new().await;
-    if ldap.is_err() {
-        panic!("{:?}", ldap.err().unwrap());
-    }
-    let mut ldap = ldap.unwrap();
-
-    // test_user(&mut ldap).await;    
-
-    // let groups = ldap.groups.to_vec().await;
-    // for group in groups {
-    //     if group.cn == "steporbite" {
-    //         println!("{:?}", group);
-    //     }
-    // }
-
-
-    let res = ldap.groups.add_group_owner("polyorbite", vec!["uid=gaetan.florio,ou=people,dc=polyorbite,dc=com"]).await;
-
-    let group = ldap.groups.group("polyorbite").await.unwrap();
-    println!("{:?}", group);
+    // run it
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:4242")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
 
-async fn test_user(ldap: &mut Ldap) {
-    let value = ldap.users.user("user_test").await.is_some();
-    let res = ldap.users.delete_user("user_test").await;
-    if res.is_err() {
-        panic!("{:?}", res.err().unwrap());
-    }
-    let res = res.unwrap();
-    println!("Delete user result: {} expected: {}", res, value);
-    
-    let new_user = UserBuilder::new().uid("user_test".to_string()).password("test".to_string()).mail("test".to_string()).last_name("test".to_string()).first_name("test".to_string()).name("test".to_string()).picture(vec![137, 80]).build();
-    if new_user.is_err() {
-        panic!("{:?}", new_user.err().unwrap());
-    }
-    let new_user = new_user.unwrap();
-
-    println!("new_user: {:?}", new_user);
-
-    let res = ldap.users.new_user(new_user).await;
-    if res.is_err() {
-        panic!("{:?}", res.err().unwrap());
-    }
-    let res = res.unwrap();
-
-    if res {
-        let user = ldap.users.user("user_test").await.unwrap();
-        println!("user: {:?}", user);
-    } else {
-        panic!("Failed to create user");
-    }
-
-    let modify = ModifyUser::new().password("password".to_string());
-    let res = ldap.users.modify_user("user_test", modify).await.unwrap();
-    println!("Modify user result: {}", res);
-
-    let user = ldap.users.user("user_test").await.unwrap();
-    println!("user: {:?}", user);
-    println!("verif : {}", user.verify_password("password"));
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
